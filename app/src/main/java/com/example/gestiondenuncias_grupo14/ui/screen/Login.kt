999d27940556ef5ff52dcc72536837896f0d888a
Esteban <esteban.morauc@gmail.com>
2025-10-12 14:23:44 -0300

    Creación de la ventana de Login (sin funcionalidad)

---
+++ app/src/main/java/com/example/gestiondenuncias_grupo14/ui/screen/Login.kt
@@ -0,0 +1,132 @@
+package com.example.gestiondenuncias_grupo14.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource   // Para cargar imágenes locales
+import com.example.gestiondenuncias_grupo14.R  // Importa el recurso R para acceder a drawable
+import androidx.compose.runtime.*     // <-- Necesario para remember, mutableStateOf, by
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Login(){
+
+    // 🔹 Variables de estado para guardar lo que el usuario escribe
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text("Inicio de Sesión (Login)") },
+                colors = topAppBarColors(
+                    containerColor = colorScheme.primary,
+                    titleContentColor = colorScheme.onPrimary
+                )
+            )
+        }
+    ) {
+        innerPadding ->
+        // 🔸 Column: organiza los elementos de manera vertical
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)              // Respeta el padding interno del Scaffold
+                .fillMaxSize()                      // Ocupa todo el espacio disponible
+                .padding(24.dp),                    // Padding uniforme alrededor
+            verticalArrangement = Arrangement.spacedBy(24.dp), // Espaciado uniforme entre elementos
+            horizontalAlignment = Alignment.CenterHorizontally // Centra los elementos horizontalmente
+        ) {
+            //Añadimos el logo
+            Image(
+                painter = painterResource(id = R.drawable.logo_empresa),
+                contentDescription = "Logo de la empresa",
+                modifier = Modifier
+                    .size(280.dp)
+                    .padding(top = 16.dp)
+            )
+
+            Text(
+                text = "Bienvenido",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary
+
+            )
+
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Nombre de usuario") },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            //Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Contraseña") },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Button(
+                onClick = { /* Acción futura */ },
+                modifier = Modifier.fillMaxWidth()
+            ) { Text("Ingresar") }
+
+            TextButton(onClick = { /*Acción futura*/ }) {
+                Text("¿No tienes cuenta? Registrate aquí")
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Todos los derechos reservados"
+            )
+
+
+        }
+    }
+}
+
+//Previsualización
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview(){
+    MaterialTheme {
+        Login()
+    }
+}
