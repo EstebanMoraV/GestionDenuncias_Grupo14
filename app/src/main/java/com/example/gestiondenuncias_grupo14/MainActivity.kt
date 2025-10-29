4cf9ee8ef2961064a1e2536a4eea0f1d2c2c839e
Esteban <esteban.morauc@gmail.com>
2025-10-28 22:34:18 -0300

    Se agrega un cambio de modo basado en el tema de tu celular, tiene modo oscuro y modo claro.

--- app/src/main/java/com/example/gestiondenuncias_grupo14/MainActivity.kt
+++ app/src/main/java/com/example/gestiondenuncias_grupo14/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.gestiondenuncias_grupo14
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
+import androidx.annotation.RequiresApi
 import androidx.navigation.compose.rememberNavController
 import com.example.gestiondenuncias_grupo14.navigation.AppNavigation
-import com.example.gestiondenuncias_grupo14.viewmodel.UsuarioViewModel
+import com.example.gestiondenuncias_grupo14.ui.theme.GestionDenuncias_Grupo14Theme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
+            GestionDenuncias_Grupo14Theme {
                 val navController = rememberNavController()
                 AppNavigation(navController = navController)
             }
--- app/src/main/java/com/example/gestiondenuncias_grupo14/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/gestiondenuncias_grupo14/navigation/AppNavigation.kt
@@ -33,7 +33,7 @@ fun AppNavigation(
 ) {
     NavHost(
         navController = navController,
-        startDestination = "relato"
+        startDestination = "login"
     ) {
         // Pantalla Login
         composable("login") {
